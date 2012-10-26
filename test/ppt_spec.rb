23708d116567e5b9ffd9549b31492795a2845043
RAVELO MARTINEZ <alu4125@cc338.(none)>
2012-10-26 15:07:58 +0100

    Planteado primer test

--- test/ppt_spec.rb
+++ test/ppt_spec.rb
@@ -1,1 +1,21 @@
 # p_ppt.rb
+
+  load './PPT.rb'
+
+
+  describe PPT do
+
+    before :all do
+
+      @appt = PPT.new("Piedra");
+
+    end
+
+    it "Debe existir " do
+
+     @ppt.respond_to("humano_tira").should == true
+     
+
+    end
+
+  end
